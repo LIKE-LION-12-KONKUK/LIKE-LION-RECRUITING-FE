dfb5e1ae1eb1de5c68d359a880ad2f6e29df6be8
sam971114 <simonana@naver.com>
2024-02-22 02:31:10 +0900

    [feat] Exhibition 이미지 import 방식 변경

--- src/components/ExhibitionPhoto.js
+++ src/components/ExhibitionPhoto.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import './ExhibitionPhoto.css';
 import '../App.css'
+import eleventhPic from "./../assets/11th.PNG";
+import twelfthPic from "./../assets/11th.PNG";
+import thirteenthPic from "./../assets/11th.PNG";
+
 
 function ExhibitionPhoto({ activeButton }) {
+  console.log("env : ", process.env.REACT_APP_PUBLIC_URL)
   let imageUrl;
 
   switch (activeButton) {
     case '11기':
       // imageUrl = 'https://drive.google.com/uc?export=view&id=19-KUmaynk6AyEMeuDT3DJHEneYgROFfu';
-      imageUrl = `${process.env.PUBLIC_URL}/exhibitionphoto/11th.png`
+      imageUrl = eleventhPic;
       break;
     case '12기':
       // imageUrl = 'https://drive.google.com/uc?export=view&id=1ygpIzYWvgAeooUZ8ahC59KImUNd-q5qX';
-      imageUrl = `${process.env.PUBLIC_URL}/exhibitionphoto/12th.png`
+      imageUrl = twelfthPic;
       break;
     case '13기':
       // imageUrl = 'https://drive.google.com/uc?export=view&id=1LuwKoGH8mh3l62nIZYeNqt_rz38kvV2t';
-      imageUrl = `${process.env.PUBLIC_URL}/exhibitionphoto/13th.png`
+      imageUrl = thirteenthPic;
       break;
     default:
       return null;
